8cc02ec3d18d06f592d95ac82cf881813a5b15df
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-09-03 02:24:59 +0600

    loadAllNews() call

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('file added');
+
+const loadAllNews = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    //console.log(response);
+    const data = await response.json();
+    //console.log(data.data);
+    return data.data;
+}
+loadAllNews();
